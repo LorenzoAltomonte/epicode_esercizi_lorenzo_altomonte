1c72ab16f0e42014e78cd8f18e012d8a9e2696c8
lorenzo altomonte <laltomonte97@gmail.com>
2022-10-10 11:28:22 +0200

    first commit

---
+++ mese1/settimana3/day1/esercizio_s3_g1/assets/js/naming.js
@@ -0,0 +1,44 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+var favoritePet="gatto";
+console.log(favoritePet);
+
+
+
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+
+var petGatto ="gatto";
+document.write(petGatto);
+
+var petCane ="cane";
+document.write(petCane);
+
+
+var petPreferito = 'criceto';
+document.getElementById('hamster').innerHTML=petPreferito;
+
+
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
+
+var numero= 12;
+console.log("il numero é " + numero);
+
+
+
+
+var numberX= 5;
+var numberY= 5;
+var result= numberX * numberY;
+document.getElementById('moltiplicazione').innerHTML=result;
